4af25ffe01413898b839989ad853013cdf11c76c
朱琛 <51479002+lantianzhidian@users.noreply.github.com>
2022-11-25 21:20:45 +0800

    Add files via upload

--- definitions.py
+++ definitions.py
@@ -1,12 +1,25 @@
+import random
+
 IMAGEDIR = "/home/zhuchencs/yanyi/aihomework/cloud_dataset/images/"
 NEWCSVDIR = "/home/zhuchencs/yanyi/aihomework/cloud_dataset_new/"
 NEWIMAGEDIR = "/home/zhuchencs/yanyi/aihomework/cloud_dataset_new/images/"
 
 RESIZE = 500
-IMG_NUM_EACH_TYPE = 2000
+IMG_NUM_EACH_TYPE = 500
 
 START = 1
-END = 29 #START + 1
+END = 29 # Type_num + 1
+
+def random_crop(img_width, img_height):
+    point_x = random.randint(int(img_width*0.3), int(img_width*0.6))
+    point_y = random.randint(0, int(img_height*0.25))
+    up = point_y
+    left = 0 if point_x - RESIZE/2 <= 0 else point_x - RESIZE/2
+    right = img_width if point_x + RESIZE/2 >= img_width else point_x + RESIZE/2
+    down = img_height if point_y + RESIZE >= img_height else point_y + RESIZE
+    #print(left, up, right, down)
+    return (left, up, right, down)
+
 
 def is_1(type):
     return 1 if type == 1 else 0
--- main.py
+++ main.py
@@ -17,98 +17,62 @@ for type in range(START,END):
             'type13', 'type14', 'type15', 'type16', 'type17', 'type18', 'type19', 'type20',\
                 'type21', 'type22', 'type23', 'type24', 'type25', 'type26', 'type27', 'type28']) # dataFrame format
     
-    #print(train_csv_n)
-    for index in train_csv_n.index: # xian ba yuan you de tupian resize, bu gou zai jia
+    
+    for index in train_csv_n.index: # resize the orginal imgs first
         '''if image_count >= IMG_NUM_EACH_TYPE:
             break'''
         image_count += 1
         img_name = train_csv_n.loc[index].values[0] # get img name of one type
-        #print(img_name)
+        
         img = Image.open(IMAGEDIR + img_name)
         out = img.resize((RESIZE, RESIZE), Image.ANTIALIAS)
         out.save(NEWIMAGEDIR + str(type) + '_' + str(image_count) + '.png', 'png') # new image name
         df.loc[image_count] = [str(type) + '_' + str(image_count) + '.png', is_1(type), is_2(type), is_3(type), is_4(type), is_5(type), is_6(type), is_7(type),\
             is_8(type), is_9(type), is_10(type),is_11(type), is_12(type), is_13(type), is_14(type), is_15(type), is_16(type), is_17(type), is_18(type), is_19(type), is_20(type),\
                 is_21(type), is_22(type), is_23(type), is_24(type), is_25(type), is_26(type), is_27(type), is_28(type)] # insert data in dataFrame
-    #df.to_csv(NEWCSVDIR + 'train.csv', mode = 'a', index = False, header = False) # write dataFrame to csv file
+    
     while (image_count < IMG_NUM_EACH_TYPE):
         for index in train_csv_n.index:
             if image_count >= IMG_NUM_EACH_TYPE:
                 break
             img_name = train_csv_n.loc[index].values[0]
             img = Image.open(IMAGEDIR + img_name)
-            # FLIP_LEFT_RIGHT
+            
+            # random_crop
             image_count += 1
-            img_l_r = img.transpose(Image.FLIP_LEFT_RIGHT)
-            out = img_l_r.resize((RESIZE, RESIZE), Image.ANTIALIAS)
+            box = random_crop(img.size[0], img.size[1])
+            img_crop = img.crop(box)
+            out = img_crop.resize((RESIZE, RESIZE), Image.ANTIALIAS)
             out.save(NEWIMAGEDIR + str(type) + '_' + str(image_count) + '.png', 'png')
             df.loc[image_count] = [str(type) + '_' + str(image_count) + '.png', is_1(type), is_2(type), is_3(type), is_4(type), is_5(type), is_6(type), is_7(type),\
                 is_8(type), is_9(type), is_10(type),is_11(type), is_12(type), is_13(type), is_14(type), is_15(type), is_16(type), is_17(type), is_18(type), is_19(type), is_20(type),\
                     is_21(type), is_22(type), is_23(type), is_24(type), is_25(type), is_26(type), is_27(type), is_28(type)] # insert data in dataFrame
-            # FLIP_LEFT_RIGHT_90
+            
+            #random_crop + FLIP_LEFT_RIGHT
             image_count += 1
-            img_l_r_90 = img_l_r.transpose(Image.ROTATE_90)
-            out = img_l_r_90.resize((RESIZE, RESIZE), Image.ANTIALIAS)
+            box = random_crop(img.size[0], img.size[1])
+            img_crop = img.crop(box)
+            img_crop_flr = img_crop.transpose(Image.FLIP_LEFT_RIGHT)
+            out = img_crop_flr.resize((RESIZE, RESIZE), Image.ANTIALIAS)
             out.save(NEWIMAGEDIR + str(type) + '_' + str(image_count) + '.png', 'png')
             df.loc[image_count] = [str(type) + '_' + str(image_count) + '.png', is_1(type), is_2(type), is_3(type), is_4(type), is_5(type), is_6(type), is_7(type),\
                 is_8(type), is_9(type), is_10(type),is_11(type), is_12(type), is_13(type), is_14(type), is_15(type), is_16(type), is_17(type), is_18(type), is_19(type), is_20(type),\
                     is_21(type), is_22(type), is_23(type), is_24(type), is_25(type), is_26(type), is_27(type), is_28(type)] # insert data in dataFrame
-            # FLIP_LEFT_RIGHT_270
+            
+            #rand_crop + FLIP_TOP_BOTTOM
             image_count += 1
-            img_l_r_270 = img_l_r.transpose(Image.ROTATE_270)
-            out = img_l_r_270.resize((RESIZE, RESIZE), Image.ANTIALIAS)
+            box = random_crop(img.size[0], img.size[1])
+            img_crop = img.crop(box)
+            img_crop_ftb = img_crop.transpose(Image.FLIP_TOP_BOTTOM)
+            out = img_crop_ftb.resize((RESIZE, RESIZE), Image.ANTIALIAS)
             out.save(NEWIMAGEDIR + str(type) + '_' + str(image_count) + '.png', 'png')
             df.loc[image_count] = [str(type) + '_' + str(image_count) + '.png', is_1(type), is_2(type), is_3(type), is_4(type), is_5(type), is_6(type), is_7(type),\
                 is_8(type), is_9(type), is_10(type),is_11(type), is_12(type), is_13(type), is_14(type), is_15(type), is_16(type), is_17(type), is_18(type), is_19(type), is_20(type),\
                     is_21(type), is_22(type), is_23(type), is_24(type), is_25(type), is_26(type), is_27(type), is_28(type)] # insert data in dataFrame
-
-            # FLIP_TOP_BOTTOM
-            image_count += 1
-            img_t_b = img.transpose(Image.FLIP_TOP_BOTTOM)
-            out = img_t_b.resize((RESIZE, RESIZE), Image.ANTIALIAS)
-            out.save(NEWIMAGEDIR + str(type) + '_' + str(image_count) + '.png', 'png')
-            df.loc[image_count] = [str(type) + '_' + str(image_count) + '.png', is_1(type), is_2(type), is_3(type), is_4(type), is_5(type), is_6(type), is_7(type),\
-                is_8(type), is_9(type), is_10(type),is_11(type), is_12(type), is_13(type), is_14(type), is_15(type), is_16(type), is_17(type), is_18(type), is_19(type), is_20(type),\
-                    is_21(type), is_22(type), is_23(type), is_24(type), is_25(type), is_26(type), is_27(type), is_28(type)] # insert data in dataFrame
-
-            # 90
-            image_count += 1
-            img_90 = img.transpose(Image.ROTATE_90)
-            out = img_90.resize((RESIZE, RESIZE), Image.ANTIALIAS)
-            out.save(NEWIMAGEDIR + str(type) + '_' + str(image_count) + '.png', 'png')
-            df.loc[image_count] = [str(type) + '_' + str(image_count) + '.png', is_1(type), is_2(type), is_3(type), is_4(type), is_5(type), is_6(type), is_7(type),\
-                is_8(type), is_9(type), is_10(type),is_11(type), is_12(type), is_13(type), is_14(type), is_15(type), is_16(type), is_17(type), is_18(type), is_19(type), is_20(type),\
-                    is_21(type), is_22(type), is_23(type), is_24(type), is_25(type), is_26(type), is_27(type), is_28(type)] # insert data in dataFrame
-
-            # 180
-            image_count += 1
-            img_180 = img.transpose(Image.ROTATE_180)
-            out = img_180.resize((RESIZE, RESIZE), Image.ANTIALIAS)
-            out.save(NEWIMAGEDIR + str(type) + '_' + str(image_count) + '.png', 'png')
-            df.loc[image_count] = [str(type) + '_' + str(image_count) + '.png', is_1(type), is_2(type), is_3(type), is_4(type), is_5(type), is_6(type), is_7(type),\
-                is_8(type), is_9(type), is_10(type),is_11(type), is_12(type), is_13(type), is_14(type), is_15(type), is_16(type), is_17(type), is_18(type), is_19(type), is_20(type),\
-                    is_21(type), is_22(type), is_23(type), is_24(type), is_25(type), is_26(type), is_27(type), is_28(type)] # insert data in dataFrame
-
-            # 270
-            image_count += 1
-            img_270 = img.transpose(Image.ROTATE_270)
-            out = img_270.resize((RESIZE, RESIZE), Image.ANTIALIAS)
-            out.save(NEWIMAGEDIR + str(type) + '_' + str(image_count) + '.png', 'png')
-            df.loc[image_count] = [str(type) + '_' + str(image_count) + '.png', is_1(type), is_2(type), is_3(type), is_4(type), is_5(type), is_6(type), is_7(type),\
-                is_8(type), is_9(type), is_10(type),is_11(type), is_12(type), is_13(type), is_14(type), is_15(type), is_16(type), is_17(type), is_18(type), is_19(type), is_20(type),\
-                    is_21(type), is_22(type), is_23(type), is_24(type), is_25(type), is_26(type), is_27(type), is_28(type)] # insert data in dataFrame
-    
+        
+        break    
+            
     df.to_csv(NEWCSVDIR + 'train.csv', mode = 'a', index = False, header = False) # write dataFrame to csv file
 
-'''
-for index in train_csv_6.index:
-    file_name = train_csv_6.loc[index].values[0]
-    img = Image.open(IMAGEDIR + file_name)
-    out = img.resize((500, 500), Image.ANTIALIAS)
-    out.save(PWD + str(index) + '.png', 'png')
-'''
-    
 
-
-#print(train_csv_6.head(133))
 
